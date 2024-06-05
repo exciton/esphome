2f7cc8f78e0e627bd50b1932780de661ebd49fb0
Niki Hammler <nhammler@apple.com>
2024-06-05 00:03:29 -0700

    Undo everything to UNIT_CELSIUS

--- esphome/components/midea_xye/climate.py
+++ esphome/components/midea_xye/climate.py
@@ -142,21 +142,21 @@ CONFIG_SCHEMA = cv.All(
                 validate_custom_fan_modes
             ),
             cv.Optional(CONF_OUTDOOR_TEMPERATURE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_FAHRENHEIT,
+                unit_of_measurement=UNIT_CELSIUS,
                 icon=ICON_THERMOMETER,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_TEMPERATURE,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_TEMPERATURE_2A): sensor.sensor_schema(
-                unit_of_measurement=UNIT_FAHRENHEIT,
+                unit_of_measurement=UNIT_CELSIUS,
                 icon=ICON_THERMOMETER,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_TEMPERATURE,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_TEMPERATURE_2B): sensor.sensor_schema(
-                unit_of_measurement=UNIT_FAHRENHEIT,
+                unit_of_measurement=UNIT_CELSIUS,
                 icon=ICON_THERMOMETER,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_TEMPERATURE,
